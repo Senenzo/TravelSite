1af46987a040088ac136ae6d472c64ad68099c93
Senenzo <mahad621@gmail.com>
2024-04-21 19:30:57 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Header
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
+
+
+//Gallery
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openImg(img) {
+  fullImgBox.style.display = "flex";
+  fullImg.src = img.src;
+}
+
+function closeImg() {
+  fullImgBox.style.display = "none";
+}
